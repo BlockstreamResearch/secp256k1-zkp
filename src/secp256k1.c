0c17f7972a907f6d71322ddb54fa99a1e095e5cd
Andrew Poelstra <apoelstra@wpsoftware.net>
2016-07-01 15:51:07 +0000

    add surjection proof module
    Includes fix and tests by Jonas Nick.

--- src/secp256k1.c
+++ src/secp256k1.c
@@ -629,3 +629,7 @@ int secp256k1_ec_pubkey_combine(const secp256k1_context* ctx, secp256k1_pubkey *
 #ifdef ENABLE_MODULE_WHITELIST
 # include "modules/whitelist/main_impl.h"
 #endif
+
+#ifdef ENABLE_MODULE_SURJECTIONPROOF
+# include "modules/surjection/main_impl.h"
+#endif
--- src/tests.c
+++ src/tests.c
@@ -5024,6 +5024,10 @@ void run_ecdsa_openssl(void) {
 # include "modules/whitelist/tests_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_SURJECTIONPROOF
+# include "modules/surjection/tests_impl.h"
+#endif
+
 int main(int argc, char **argv) {
     unsigned char seed16[16] = {0};
     unsigned char run32[32] = {0};
@@ -5158,6 +5162,10 @@ int main(int argc, char **argv) {
 #ifdef ENABLE_MODULE_WHITELIST
     /* Key whitelisting tests */
     run_whitelist_tests();
+#endif
+
+#ifdef ENABLE_MODULE_SURJECTIONPROOF
+    run_surjection_tests();
 #endif
 
     secp256k1_rand256(run32);
