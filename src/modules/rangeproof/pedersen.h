f6c84a02f331acb26adae9caee635b059ea70107
Pieter Wuille <pieter.wuille@gmail.com>
2016-07-06 13:46:23 +0200

    Expose generator in pedersen/rangeproof API

--- src/modules/rangeproof/pedersen.h
+++ src/modules/rangeproof/pedersen.h
@@ -14,9 +14,9 @@
 #include <stdint.h>
 
 /** Multiply a small number with the generator: r = gn*G2 */
-static void secp256k1_pedersen_ecmult_small(secp256k1_gej *r, uint64_t gn);
+static void secp256k1_pedersen_ecmult_small(secp256k1_gej *r, uint64_t gn, const secp256k1_ge* genp);
 
 /* sec * G + value * G2. */
-static void secp256k1_pedersen_ecmult(const secp256k1_ecmult_gen_context *ecmult_gen_ctx, secp256k1_gej *rj, const secp256k1_scalar *sec, uint64_t value);
+static void secp256k1_pedersen_ecmult(const secp256k1_ecmult_gen_context *ecmult_gen_ctx, secp256k1_gej *rj, const secp256k1_scalar *sec, uint64_t value, const secp256k1_ge* genp);
 
 #endif
