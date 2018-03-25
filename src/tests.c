72ffc029a320f729fbfcffd0692abe00e2ccd438
Andrew Poelstra <apoelstra@wpsoftware.net>
2018-03-25 15:27:35 +0000

    bulletproofs: add module, full support for rangeproofs

--- src/tests.c
+++ src/tests.c
@@ -5143,6 +5143,10 @@ void run_ecdsa_openssl(void) {
 # include "modules/rangeproof/tests_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_BULLETPROOF
+# include "modules/bulletproofs/tests_impl.h"
+#endif
+
 #ifdef ENABLE_MODULE_WHITELIST
 # include "modules/whitelist/tests_impl.h"
 #endif
@@ -5289,6 +5293,10 @@ int main(int argc, char **argv) {
 
 #ifdef ENABLE_MODULE_RANGEPROOF
     run_rangeproof_tests();
+#endif
+
+#ifdef ENABLE_MODULE_BULLETPROOF
+    run_bulletproofs_tests();
 #endif
 
 #ifdef ENABLE_MODULE_WHITELIST
