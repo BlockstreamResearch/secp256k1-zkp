ymmah/secp256k1-mw

bulletproofs: add a bunch of binary example circuits

src/modules/bulletproof/tests_impl.h
@@ -537,9 +537,12 @@ void test_bulletproof_circuit(const secp256k1_ge *gens, const size_t n_gens) {
     const char inv_17_19_circ[] = "2,0,0,4; L0 = 17; 2*L1 - L0 = 21; O0 = 1; O1 = 1;";
     secp256k1_bulletproof_circuit *simple = secp256k1_parse_circuit(ctx, inv_17_19_circ);
     secp256k1_bulletproof_circuit *pedersen_3 = secp256k1_parse_circuit(ctx, pedersen_3_desc);
+    secp256k1_bulletproof_circuit *pedersen_3_bin = secp256k1_bulletproof_circuit_decode(ctx, "src/modules/bulletproof/bin_circuits/pedersen-3.circ");
+    secp256k1_bulletproof_circuit_assignment *pedersen_3_assn = secp256k1_bulletproof_circuit_assignment_decode(ctx, "src/modules/bulletproof/bin_circuits/pedersen-3.assn");
 
     CHECK(simple != NULL);
     CHECK(pedersen_3 != NULL);
+    CHECK(pedersen_3_bin != NULL);
 
     CHECK(secp256k1_scratch_allocate_frame (scratch, 3072 * sizeof(secp256k1_scalar), 3));
     assn.al = secp256k1_scratch_alloc(scratch, 1024 * sizeof(*assn.al));
@@ -618,8 +621,35 @@ void test_bulletproof_circuit(const secp256k1_ge *gens, const size_t n_gens) {
         NULL, 0
     ));
 
+    plen = 2000;
+    CHECK(secp256k1_bulletproof_relation66_prove_impl(
+        &ctx->ecmult_ctx,
+        scratch,
+        proof, &plen,
+        pedersen_3_assn,
+        NULL, NULL, 0,
+        &secp256k1_ge_const_g2,
+        pedersen_3_bin,
+        gens, n_gens,
+        nonce,
+        NULL, 0
+    ));
+
+    CHECK(secp256k1_bulletproof_relation66_verify_impl(
+        &ctx->ecmult_ctx,
+        scratch,
+        &proof_ptr, 1, plen,
+        NULL, 0,
+        &secp256k1_ge_const_g2,
+        (const secp256k1_bulletproof_circuit* const*) &pedersen_3_bin,
+        gens, n_gens,
+        NULL, 0
+    ));
+
     secp256k1_bulletproof_circuit_destroy(ctx, simple);
     secp256k1_bulletproof_circuit_destroy(ctx, pedersen_3);
+    secp256k1_bulletproof_circuit_destroy(ctx, pedersen_3_bin);
+    secp256k1_bulletproof_circuit_assignment_destroy(ctx, pedersen_3_assn);
     secp256k1_scratch_deallocate_frame(scratch);
     secp256k1_scratch_destroy(scratch);
 }

BlockstreamResearch/secp256k1-zkp

src/modules/bulletproofs/tests_impl.h
@@ -643,9 +643,12 @@ void test_bulletproof_circuit(const secp256k1_bulletproof_generators *gens) {
     const char inv_17_19_circ[] = "2,1,0,5; L0 = 17; 2*L1 - L0 = 21; O0 = 1; O1 = 1; V0 - L0 = 100;";
     secp256k1_bulletproof_circuit *simple = secp256k1_parse_circuit(ctx, inv_17_19_circ);
     secp256k1_bulletproof_circuit *pedersen_3 = secp256k1_parse_circuit(ctx, pedersen_3_desc);
+    secp256k1_bulletproof_circuit *pedersen_3_bin = secp256k1_bulletproof_circuit_decode(ctx, "src/modules/bulletproofs/bin_circuits/pedersen-3.circ");
+    secp256k1_bulletproof_circuit_assignment *pedersen_3_assn = secp256k1_bulletproof_circuit_assignment_decode(ctx, "src/modules/bulletproofs/bin_circuits/pedersen-3.assn");
 
     CHECK(simple != NULL);
     CHECK(pedersen_3 != NULL);
+    CHECK(pedersen_3_bin != NULL);
 
     CHECK(secp256k1_scratch_allocate_frame (scratch, 3072 * sizeof(secp256k1_scalar), 3));
     assn.al = secp256k1_scratch_alloc(scratch, 1024 * sizeof(*assn.al));
@@ -732,8 +735,35 @@ void test_bulletproof_circuit(const secp256k1_bulletproof_generators *gens) {
         NULL, 0
     ));
 
+    plen = 2000;
+    CHECK(secp256k1_bulletproof_relation66_prove_impl(
+        &ctx->ecmult_ctx,
+        scratch,
+        proof, &plen,
+        pedersen_3_assn,
+        NULL, NULL, 0,
+        &value_gen,
+        pedersen_3_bin,
+        gens,
+        nonce,
+        NULL, 0
+    ));
+
+    CHECK(secp256k1_bulletproof_relation66_verify_impl(
+        &ctx->ecmult_ctx,
+        scratch,
+        &proof_ptr, 1, plen,
+        NULL, NULL,
+        &value_gen,
+        (const secp256k1_bulletproof_circuit* const*) &pedersen_3_bin,
+        gens,
+        NULL, 0
+    ));
+
     secp256k1_bulletproof_circuit_destroy(ctx, simple);
     secp256k1_bulletproof_circuit_destroy(ctx, pedersen_3);
+    secp256k1_bulletproof_circuit_destroy(ctx, pedersen_3_bin);
+    secp256k1_bulletproof_circuit_assignment_destroy(ctx, pedersen_3_assn);
     secp256k1_scratch_deallocate_frame(scratch);
     secp256k1_scratch_destroy(scratch);
 }