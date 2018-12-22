77d5b4ac7dc2eaea96fdd75636bd2dc621e4104b
Jonas Nick <jonasd.nick@gmail.com>
2018-12-22 22:12:35 +0000

    Add MuSig module which allows creating n-of-n multisignatures and adaptor signatures.

--- src/secp256k1.c
+++ src/secp256k1.c
@@ -639,6 +639,10 @@ int secp256k1_ec_pubkey_combine(const secp256k1_context* ctx, secp256k1_pubkey *
 # include "modules/schnorrsig/main_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_MUSIG
+# include "modules/musig/main_impl.h"
+#endif
+
 #ifdef ENABLE_MODULE_RECOVERY
 # include "modules/recovery/main_impl.h"
 #endif
--- src/tests.c
+++ src/tests.c
@@ -5123,6 +5123,10 @@ void run_ecdsa_openssl(void) {
 # include "modules/schnorrsig/tests_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_MUSIG
+# include "modules/musig/tests_impl.h"
+#endif
+
 #ifdef ENABLE_MODULE_RECOVERY
 # include "modules/recovery/tests_impl.h"
 #endif
@@ -5254,6 +5258,10 @@ int main(int argc, char **argv) {
 #ifdef ENABLE_MODULE_SCHNORRSIG
     /* Schnorrsig tests */
     run_schnorrsig_tests();
+#endif
+
+#ifdef ENABLE_MODULE_MUSIG
+    run_musig_tests();
 #endif
 
     /* ecdsa tests */
