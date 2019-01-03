e3c3376e6ac238b65755969c19862d8363c2e62f
Gregory Sanders <gsanders87@gmail.com>
2019-01-03 13:45:36 -0500

    Enable more builds with rest of experimental flags

--- contrib/travis.sh
+++ contrib/travis.sh
@@ -20,6 +20,7 @@ fi
     --enable-experimental="$EXPERIMENTAL" --enable-endomorphism="$ENDOMORPHISM" \
     --with-field="$FIELD" --with-bignum="$BIGNUM" --with-asm="$ASM" --with-scalar="$SCALAR" \
     --enable-ecmult-static-precomputation="$STATICPRECOMPUTATION" --with-ecmult-gen-precision="$ECMULTGENPRECISION" \
+    --enable-module-recovery=$RECOVERY --enable-module-rangeproof=$RANGEPROOF --enable-module-whitelist=$WHITELIST --enable-module-generator=$GENERATOR \
     --enable-module-ecdh="$ECDH" --enable-module-recovery="$RECOVERY" "$EXTRAFLAGS" "$USE_HOST"
 
 if [ -n "$BUILD" ]
