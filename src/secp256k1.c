a4abd088983e2582ad8c5146a84b4cc685a177b4
Jonas Nick <jonasd.nick@gmail.com>
2020-05-12 13:58:47 +0000

    extrakeys: Init empty experimental module
    This is to prepare for xonly_pubkeys and keypairs.

--- src/secp256k1.c
+++ src/secp256k1.c
@@ -799,3 +799,7 @@ int secp256k1_ec_pubkey_combine(const secp256k1_context* ctx, secp256k1_pubkey *
 #ifdef ENABLE_MODULE_SURJECTIONPROOF
 # include "modules/surjection/main_impl.h"
 #endif
+
+#ifdef ENABLE_MODULE_EXTRAKEYS
+# include "modules/extrakeys/main_impl.h"
+#endif
--- src/tests.c
+++ src/tests.c
@@ -5459,6 +5459,10 @@ void run_ecdsa_openssl(void) {
 # include "modules/surjection/tests_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_EXTRAKEYS
+# include "modules/extrakeys/tests_impl.h"
+#endif
+
 void run_memczero_test(void) {
     unsigned char buf1[6] = {1, 2, 3, 4, 5, 6};
     unsigned char buf2[sizeof(buf1)];
@@ -5790,6 +5794,10 @@ int main(int argc, char **argv) {
 
 #ifdef ENABLE_MODULE_SURJECTIONPROOF
     run_surjection_tests();
+#endif
+
+#ifdef ENABLE_MODULE_EXTRAKEYS
+    run_extrakeys_tests();
 #endif
 
     /* util tests */
