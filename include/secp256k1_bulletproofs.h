2cc4261087b1683d36a3fce3b8cb4cb268a65f59
Andrew Poelstra <apoelstra@wpsoftware.net>
2022-08-27 15:02:44 +0000

    bulletproofs: add new empty module

---
+++ include/secp256k1_bulletproofs.h
@@ -0,0 +1,18 @@
+#ifndef _SECP256K1_BULLETPROOFS_
+# define _SECP256K1_BULLETPROOFS_
+
+# include "secp256k1.h"
+
+# ifdef __cplusplus
+extern "C" {
+# endif
+
+#include <stdint.h>
+
+/* TODO */
+
+# ifdef __cplusplus
+}
+# endif
+
+#endif
