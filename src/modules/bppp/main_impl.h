34d641b36aa9adbe06d6c0e3ff9468f1faa6d229
sanket1729 <sanket1729@gmail.com>
2022-11-30 22:19:32 -0800

    Update generator serialization to have h0 = G
    For compatibility with exisiting Pedersen commitments data structures,
    it is necessary to have commitments be of the G_a*v + G*gamma where G_a
    is asset gen and gamma is blinding factors.
    
    However, in BP++ design, the blinding values are along H_vec. In order
    make these compatible with BP++, we make h0 = G

--- src/modules/bppp/main_impl.h
+++ src/modules/bppp/main_impl.h
@@ -24,6 +24,11 @@ secp256k1_bppp_generators *secp256k1_bppp_generators_create(const secp256k1_cont
 
     VERIFY_CHECK(ctx != NULL);
 
+    /* Must have atleast 8 generators */
+    if (n <= 8) {
+        return NULL;
+    }
+
     ret = (secp256k1_bppp_generators *)checked_malloc(&ctx->error_callback, sizeof(*ret));
     if (ret == NULL) {
         return NULL;
@@ -42,6 +47,11 @@ secp256k1_bppp_generators *secp256k1_bppp_generators_create(const secp256k1_cont
     for (i = 0; i < n; i++) {
         secp256k1_generator gen;
         unsigned char tmp[32] = { 0 };
+        if (i == n - 8) {
+            /* The first generator in H is secp G */
+            ret->gens[i] = secp256k1_ge_const_g;
+            continue;
+        }
         secp256k1_rfc6979_hmac_sha256_generate(&rng, tmp, 32);
         CHECK(secp256k1_generator_generate(ctx, &gen, tmp));
         secp256k1_generator_load(&ret->gens[i], &gen);
@@ -51,13 +61,15 @@ secp256k1_bppp_generators *secp256k1_bppp_generators_create(const secp256k1_cont
 }
 
 secp256k1_bppp_generators* secp256k1_bppp_generators_parse(const secp256k1_context* ctx, const unsigned char* data, size_t data_len) {
-    size_t n = data_len / 33;
+    /* Allocate an extra generator for the H0 = G value */
+    size_t n = data_len / 33 + 1;
+    size_t i = 0, j = 0;
     secp256k1_bppp_generators* ret;
 
     VERIFY_CHECK(ctx != NULL);
     ARG_CHECK(data != NULL);
 
-    if (data_len % 33 != 0) {
+    if (data_len % 33 != 0 || n <= 8) {
         return NULL;
     }
 
@@ -65,42 +77,57 @@ secp256k1_bppp_generators* secp256k1_bppp_generators_parse(const secp256k1_conte
     if (ret == NULL) {
         return NULL;
     }
-    ret->n = n;
+
     ret->gens = (secp256k1_ge*)checked_malloc(&ctx->error_callback, n * sizeof(*ret->gens));
     if (ret->gens == NULL) {
         free(ret);
         return NULL;
     }
 
-    while (n--) {
+    for (i = 0; i < n; i++) {
         secp256k1_generator gen;
-        if (!secp256k1_generator_parse(ctx, &gen, &data[33 * n])) {
+        if (i == n - 8) {
+            /* The first generator in H is secp G */
+            ret->gens[i] = secp256k1_ge_const_g;
+            continue;
+        }
+        if (!secp256k1_generator_parse(ctx, &gen, &data[33 * j])) {
             free(ret->gens);
             free(ret);
             return NULL;
         }
-        secp256k1_generator_load(&ret->gens[n], &gen);
+        secp256k1_generator_load(&ret->gens[i], &gen);
+        j++;
     }
+    ret -> n = n;
     return ret;
 }
 
 int secp256k1_bppp_generators_serialize(const secp256k1_context* ctx, const secp256k1_bppp_generators* gens, unsigned char* data, size_t *data_len) {
-    size_t i;
+    size_t i, j = 0;
 
     VERIFY_CHECK(ctx != NULL);
     ARG_CHECK(gens != NULL);
     ARG_CHECK(data != NULL);
     ARG_CHECK(data_len != NULL);
-    ARG_CHECK(*data_len >= 33 * gens->n);
+    ARG_CHECK(*data_len >= 33 * (gens->n - 1));
 
     memset(data, 0, *data_len);
+    if (*data_len < 33 * (gens->n - 1) || gens->n <= 8) {
+        return 0;
+    }
     for (i = 0; i < gens->n; i++) {
         secp256k1_generator gen;
+        if (i == gens->n - 8) {
+            /* The first generator in H is secp G */
+            continue;
+        }
         secp256k1_generator_save(&gen, &gens->gens[i]);
-        secp256k1_generator_serialize(ctx, &data[33 * i], &gen);
+        secp256k1_generator_serialize(ctx, &data[33 * j], &gen);
+        j++;
     }
 
-    *data_len = 33 * gens->n;
+    *data_len = 33 * (gens->n - 1);
     return 1;
 }
 
@@ -111,6 +138,30 @@ void secp256k1_bppp_generators_destroy(const secp256k1_context* ctx, secp256k1_b
         free(gens->gens);
         free(gens);
     }
+}
+
+size_t secp256k1_bulletproofs_pp_rangeproof_proof_length(
+    const secp256k1_context* ctx,
+    size_t n_bits,
+    size_t base
+) {
+    size_t num_digits, n_rounds, g_len, h_len;
+    VERIFY_CHECK(ctx != NULL);
+    if (n_bits > 64 || base < 2 || base > 64) {
+        return 0;
+    }
+
+    if (!secp256k1_is_power_of_two(base) || !secp256k1_is_power_of_two(n_bits)) {
+        return 0;
+    }
+    num_digits = n_bits / secp256k1_bppp_log2(base);
+    if (!secp256k1_is_power_of_two(num_digits)) {
+        return 0;
+    }
+    g_len = num_digits > base ? num_digits : base;
+    h_len = 8;
+    n_rounds = secp256k1_bppp_log2(g_len > h_len ? g_len : h_len);
+    return 33 * 4 + 65*n_rounds + 64;
 }
 
 #endif
