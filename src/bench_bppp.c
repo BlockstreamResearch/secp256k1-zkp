d4b3a8eca60b72a4a21c2902d5df72929d0ee3d8
Jonas Nick <jonasd.nick@gmail.com>
2023-02-11 17:22:14 +0000

    bppp: avoid benchmarking verification of same proof over and over

--- src/bench_bppp.c
+++ src/bench_bppp.c
@@ -11,12 +11,14 @@
 #include "util.h"
 #include "bench.h"
 
+#define MAX_PROOF_SIZE 500
+
 typedef struct {
     secp256k1_context* ctx;
     secp256k1_bppp_generators* gens;
     secp256k1_scratch_space *scratch;
     secp256k1_pedersen_commitment commit;
-    unsigned char proof[1000];
+    unsigned char *proofs;
     unsigned char blind[32];
     unsigned char nonce[32];
     size_t proof_len;
@@ -31,13 +33,13 @@ static void bench_bppp_setup(void* arg) {
 
     data->min_value = 0;
     data->value = 100;
-    data->proof_len = sizeof(data->proof);
+    data->proof_len = MAX_PROOF_SIZE;
     memset(data->blind, 0x77, 32);
     memset(data->nonce, 0x0, 32);
     CHECK(secp256k1_pedersen_commit(data->ctx, &data->commit, data->blind, data->value, secp256k1_generator_h));
 
-    CHECK(secp256k1_bppp_rangeproof_prove(data->ctx, data->scratch, data->gens, secp256k1_generator_h, data->proof, &data->proof_len, data->n_bits, data->base, data->value, 0, &data->commit, data->blind, data->nonce, NULL, 0));
-    CHECK(secp256k1_bppp_rangeproof_verify(data->ctx, data->scratch, data->gens, secp256k1_generator_h, data->proof, data->proof_len, data->n_bits, data->base, data->min_value, &data->commit, NULL, 0));
+    CHECK(secp256k1_bppp_rangeproof_prove(data->ctx, data->scratch, data->gens, secp256k1_generator_h, data->proofs, &data->proof_len, data->n_bits, data->base, data->value, 0, &data->commit, data->blind, data->nonce, NULL, 0));
+    CHECK(secp256k1_bppp_rangeproof_verify(data->ctx, data->scratch, data->gens, secp256k1_generator_h, data->proofs, data->proof_len, data->n_bits, data->base, data->min_value, &data->commit, NULL, 0));
 }
 
 static void bench_bppp_prove(void* arg, int iters) {
@@ -48,8 +50,8 @@ static void bench_bppp_prove(void* arg, int iters) {
         data->nonce[1] = i;
         data->nonce[2] = i >> 8;
         data->nonce[3] = i >> 16;
-        data->proof_len = sizeof(data->proof);
-        CHECK(secp256k1_bppp_rangeproof_prove(data->ctx, data->scratch, data->gens, secp256k1_generator_h, data->proof, &data->proof_len, data->n_bits, data->base, data->value, 0, &data->commit, data->blind, data->nonce, NULL, 0));
+        data->proof_len = MAX_PROOF_SIZE;
+        CHECK(secp256k1_bppp_rangeproof_prove(data->ctx, data->scratch, data->gens, secp256k1_generator_h, &data->proofs[i*MAX_PROOF_SIZE], &data->proof_len, data->n_bits, data->base, data->value, 0, &data->commit, data->blind, data->nonce, NULL, 0));
     }
 }
 
@@ -58,28 +60,30 @@ static void bench_bppp_verify(void* arg, int iters) {
     int i;
 
     for (i = 0; i < iters; i++) {
-        CHECK(secp256k1_bppp_rangeproof_verify(data->ctx, data->scratch, data->gens, secp256k1_generator_h, data->proof, data->proof_len, data->n_bits, data->base, data->min_value, &data->commit, NULL, 0));
+        CHECK(secp256k1_bppp_rangeproof_verify(data->ctx, data->scratch, data->gens, secp256k1_generator_h, &data->proofs[i*MAX_PROOF_SIZE], data->proof_len, data->n_bits, data->base, data->min_value, &data->commit, NULL, 0));
     }
 }
 
 int main(void) {
     bench_bppp_data data;
-    int iters = get_iters(32);
+    int iters = get_iters(64);
     char test_name[64];
 
     data.ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
     data.gens = secp256k1_bppp_generators_create(data.ctx, 24);
-    data.scratch = secp256k1_scratch_space_create(data.ctx, 8000 * 1024);
+    data.scratch = secp256k1_scratch_space_create(data.ctx, 80 * 1024);
+    data.proofs = (unsigned char *)malloc(iters * MAX_PROOF_SIZE);
 
     data.n_bits = 1ul << 6;
     data.base = 16;
     sprintf(test_name, "bppp_prove_64bits_16base");
-    run_benchmark(test_name, bench_bppp_prove, bench_bppp_setup, NULL, &data, 20, iters);
+    run_benchmark(test_name, bench_bppp_prove, bench_bppp_setup, NULL, &data, 4, iters);
 
     sprintf(test_name, "bppp_verify_64bits_16base");
     run_benchmark(test_name, bench_bppp_verify, bench_bppp_setup, NULL, &data, 20, iters);
 
     secp256k1_scratch_space_destroy(data.ctx, data.scratch);
+    free(data.proofs);
     secp256k1_bppp_generators_destroy(data.ctx, data.gens);
     secp256k1_context_destroy(data.ctx);
 
