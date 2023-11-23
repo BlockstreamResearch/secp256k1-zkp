6f47d2eb22a4fe98a6e8a400b2e681eca4c38ca4
Jesse Posner <jesse.posner@gmail.com>
2023-11-23 10:57:08 -0700

    frost trusted dealer: initialize project
    This commit adds the foundational configuration and building scripts
    and an initial structure for the project.

---
+++ include/secp256k1_frost.h
@@ -0,0 +1,24 @@
+#ifndef SECP256K1_FROST_H
+#define SECP256K1_FROST_H
+
+#include "secp256k1_extrakeys.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+/** This code is currently a work in progress. It's not secure nor stable.  IT
+ * IS EXTREMELY DANGEROUS AND RECKLESS TO USE THIS MODULE IN PRODUCTION!
+
+ * This module implements a variant of Flexible Round-Optimized Schnorr
+ * Threshold Signatures (FROST) by Chelsea Komlo and Ian Goldberg
+ * (https://crysp.uwaterloo.ca/software/frost/).
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
