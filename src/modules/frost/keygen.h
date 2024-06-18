2336b02fad5902b7049892a4b7627aef013e5b2a
Jesse Posner <jesse.posner@gmail.com>
2024-06-17 18:10:13 -0700

    frost: share generation
    This commit adds share generation, as well as share serialization and
    parsing.

--- src/modules/frost/keygen.h
+++ src/modules/frost/keygen.h
@@ -4,9 +4,10 @@
  * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
  **********************************************************************/
 
-#ifndef SECP256K1_MODULE_FROST_MAIN
-#define SECP256K1_MODULE_FROST_MAIN
+#ifndef SECP256K1_MODULE_FROST_KEYGEN_H
+#define SECP256K1_MODULE_FROST_KEYGEN_H
 
-#include "keygen_impl.h"
+#include "../../../include/secp256k1.h"
+#include "../../../include/secp256k1_frost.h"
 
 #endif
